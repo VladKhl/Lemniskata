4b70883906e265bc63ed428e1520667d044f0cb4
Владислав Хлыбов <vavan8765@gmail.com>
2022-02-22 11:09:11 +0300

    Добавьте файлы проекта.

---
+++ Lemniskata/Lemniskata/StartPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+using Xamarin.Forms.Xaml;
+
+namespace Lemniskata
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StartPage : Xamarin.Forms.TabbedPage
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+            On<Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
+        }
+    }
+}
